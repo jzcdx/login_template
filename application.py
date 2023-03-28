cc3c3d07d3f97882b06e167e38200b41bb18323c
MicroOptimization <jackiezhen538@gmail.com>
2023-03-28 13:19:55 -0400

    first

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+@application.route("/")
+def main():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    application.run(debug=True, use_reloader=True, threaded=True)
