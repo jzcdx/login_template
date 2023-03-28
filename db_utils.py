fa66b00e28d77db01ab481691d299048cab88c6c
MicroOptimization <jackiezhen538@gmail.com>
2023-03-28 19:00:20 -0400

    password checking and retrieval from email works on backend

--- db_utils.py
+++ db_utils.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, URL, text, Table, MetaData, Column, Integer, String, Double, select, delete, update, insert
 import os
+import bcrypt
 
 class DBH: #database helper
     def __init__(self):
@@ -26,25 +27,54 @@ class DBH: #database helper
             stmt = select(self.user_account);
             res = conn.execute(stmt);
             rows = res.mappings().all()
+            conn.close()
             print(rows);
             return rows;
 
-    def get_password(self, email): #userId
+    def read_password(self, email): #userId
         with self.engine.connect() as conn:
             stmt = select(self.user_account).where(self.user_account.c.email == email);
             res = conn.execute(stmt);
             rows = res.mappings().all();
+            conn.close()
             if rows:
-                return rows[0] #returns a single dict if the email exists in your database
+                return rows[0]["password"] #returns a single dict if the email exists in your database
             return None 
     
-    """
-    def create_account():
+    
+    def create_account(self, new_email, new_password):
         with self.engine.connect() as conn:
-            stmt = 
-    """
+            #we're gonna check if the email exists in our system first
+            stmt = select(self.user_account).where(self.user_account.c.email == new_email);
+            res = conn.execute(stmt);
+            rows = res.mappings().all();
+
+            if rows: #so the email is taken
+                return False #this is whether or not the account creation succeeded
+            
+            #don't forget to hash those passwords
+            salt = bcrypt.gensalt() 
+            hashed = bcrypt.hashpw(new_password.encode('utf8'), salt)
+            hashed = hashed.decode()
+
+            stmt = insert(self.user_account).values(
+                email = new_email,
+                password = hashed
+            ).returning(self.user_account.c.id)
+
+            new_id = conn.execute(stmt)
+            print("created account, new id: " , new_id.mappings().all())
+            conn.commit();
+            return True #acc creation succeeded
+
 dbh = DBH()
 
-res = dbh.get_password("asdff@gmail.com");
+#print("Creating account, Success: " , dbh.create_account("bearsley@gmail.com", "iambearsley"))
 
+res = dbh.read_password("bearsley@gmail.com");
+res = dbh.read_password("asdf@gmail.com")
+print(res)
+pw_input = "iambearsley"
+pw_input = "testpassword7"
+print("checking pw: " , bcrypt.checkpw(pw_input.encode('utf8'), res.encode('utf8')))
 
