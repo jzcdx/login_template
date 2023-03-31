63c1e912d9c3709ff841a0e2ad98e9135b29c928
MicroOptimization <jackiezhen538@gmail.com>
2023-03-31 14:42:49 -0400

    code cleanup

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,20 +1,14 @@
-function test() {
-    alert("hi");
-}
-
 var signUpDiv = document.getElementById("sign-up");
 var signInDiv = document.getElementById("sign-in");
 var header = document.getElementById("header");
 
 function showSignUp() {
-    console.log("here");
     signUpDiv.style.display = "block";
     signInDiv.style.display = "none";
     header.innerHTML = "sign up"
 }
 
 function submit_new_account() {
-    console.log("heeeeere");
     var newEmail = document.getElementById("newEmail").value;
     var newPassword = document.getElementById("newPassword").value;
     var newPasswordC = document.getElementById("newPasswordC").value;
@@ -35,8 +29,6 @@ function submit_new_account() {
         contentType: "application/json",
         dataType: 'json', 
         success: function(result) { //when the response comes back and it's successful, run the code below
-            //This updates the description with the new value from the textarea
-            //console.log(result)
             backToLogin()
         } 
     });
